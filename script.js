92d7a79838c5a51344cdc67369e84c7077e5f5f8
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2018-08-31 08:33:08 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function move() {
+	document.getElementById("photo").src="file:///C:/Users/Lima 136 Alpha Delta/Desktop/sdfghjk/Untitled.png";
+};
+function howard() {
+	document.getElementById("howd").src="https://www.youtube.com/embed/dOQmE-BgVgA?rel=0&amp;controls=0";
+};
