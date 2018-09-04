0b7a0a500b682e86991decd076fbecbdc79f8ee4
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2018-09-04 12:41:34 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function move() {
-	document.getElementById("photo").src="lockness-Ko.github.io/download.jpg";
+	document.getElementById("photo").src="https://lockness-ko.github.io/download.jpg";
 };
 function howard() {
 	document.getElementById("howd").src="https://www.youtube.com/embed/dOQmE-BgVgA?rel=0&amp;controls=0";
