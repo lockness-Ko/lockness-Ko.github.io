bb9b56109bbbfb1a489f2679d0d6d4f020f9ad24
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2018-09-28 13:11:59 +1000

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function move() {
-	document.getElementById("photo").src="https://lockness-ko.github.io/download.jpg";
-};
-function howard() {
-	document.getElementById("howd").src="https://www.youtube.com/embed/dOQmE-BgVgA?rel=0&amp;controls=0";
-};
