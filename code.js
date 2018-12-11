8dfec93fa084d9469c538d66cc91ce539a10e1c9
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2018-12-12 10:20:05 +1100

    Update and rename code.js to main.js

--- code.js
+++
@@ -1,7 +0,0 @@
-function log(){
-    console.log("Username=" + document.getElementById("INPUT_USERNAME_AUTH").value + " Password=" + document.getElementById("INPUT_PASSWORD_AUTH").value);
-    var cheese = document.getElementById("cheese");
-    var auth = document.getElementById("clickclock");
-    clickclock.innerHTML = "Incorrect Authentification";
-    cheese.innerHTML = document.getElementById("INPUT_USERNAME_AUTH").value + " and " + document.getElementById("INPUT_PASSWORD_AUTH").value;
-}
